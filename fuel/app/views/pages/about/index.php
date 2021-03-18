b5700a34a65f8a841f0486738a24cc8e9e04ee7c
Cayce Kelley <46986113+ospreytoon3@users.noreply.github.com>
2021-03-17 20:29:09 -0600

    Added images

--- fuel/app/views/pages/about/index.php
+++ fuel/app/views/pages/about/index.php
@@ -5,9 +5,11 @@
     <main>
         <h2>Cayce Kelley</h2>    
             <p>I'm a third-year CS student here at Colorado State University.</p>
+            <?php Asset::img('Cayce.jpg', array('style' => 'width: 400px')) ?>
 
         <h2>Caleb Cluett</h2> 
             <p>I'm a Juniter in the Computer Science program. I'm also a member of the CSU Climbing Team</p>
+            <?php Asset::img('Caleb.jpg', array('style' => 'width: 400px')) ?>
 
         <h2>James West</h2> 
             <p>Im a junior at Colorado State and I enjoy to snowboard</p>
