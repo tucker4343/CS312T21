02e7f62b2a104ff3126fc07e4264109777984175
tucker4343 <99790987+tucker4343@users.noreply.github.com>
2022-03-30 22:41:03 -0600

    Update index.php

--- fuel/app/views/m1/about/index.php
+++ fuel/app/views/m1/about/index.php
@@ -1,19 +1,32 @@
-<body>
-    <header>
-        Welcome to the About page
-    </header>
-    <main>
-        <h2>Caleb Cluett</h2> 
-            <?php echo Asset::img('Caleb.jpg', array('style' => 'width: 200px')) ?>
-            <p>I'm a Juniter in the Computer Science program. I'm also a member of the CSU Climbing Team</p>
-            <br></br>
-        <h2>James West</h2> 
-            <?php echo Asset::img('James.jpeg', array('style' => 'width: 200px')) ?>
-            <p>Im a junior at Colorado State and I enjoy to snowboard</p>
-            <br></br>
-        <h2>Cayce Kelley</h2>    
-            <?php echo Asset::img('Cayce.jpg', array('style' => 'width: 200px')) ?>
-            <p>I'm a third-year CS student here at Colorado State University.</p>
-            <br></br>
-    </main>
-</body>
+<table>
+    <h1>Meet the Team!</h1>
+    <!---CASEY--->
+    <tr>
+        <th><?php echo Asset::img('casey.png',array('id'=>'profile')) ?></th>
+        <td>
+            <div id="name">Casey Roth</div> 
+            <div id="major">Major: Computer Science Human Centered Computing Concentration</div>
+            <p>In his free time Casey likes to play card games and video games with his friends. Graphic design is his passion and he is the creator of the ColorfulCreations LLC logo. In the future, Casey wants to enter the games industry and develope his own video games. Keep your eye out for any projects with his name attached to them!
+
+        </td>
+    </tr>  
+    <tr>
+        <th><?php //echo Asset::img('fores.png',array('id'=>'profile')) ?></th>
+        <td>
+            <div id="name">Fores Yousifauni</div> 
+            <div id="major">Major: INSERT HERE</div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </td>
+    </tr>  
+    <tr>
+        <th><?php //echo Asset::img('tucker.png',array('id'=>'profile')) ?></th>
+        <td>
+            <div id="name">Tucker Martin</div> 
+            <div id="major">Major: Computer Science</div>
+            <p>I am a computer science major at CSU who loves to ski and be outside as much as a I can. I also love sports and hope to one day work with computer science in a sports setting.
+        </td>
+    </tr>
+    
+
+
+</table>
